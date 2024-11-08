a896b65776fd7fc6b135460d8d21bf13d6d85c89
Rudranarayan Sahoo <rudraomm2005@gmail.com>
2024-11-08 19:02:21 +0530

    Add files via upload

---
+++ Arithmatic_Operation/Addition2Number.py
@@ -0,0 +1,9 @@
+#Arithmatic Operation - Addition Two Numbers..
+
+
+#User-Input - User Enter Two Numbers 
+Num1 = float(input("Enter First Number: "))
+Num2 = float(input("Enter Second Number: "))
+
+#Print The Addition Value Of Two Inputed Numbers
+print(f"The Sum of {Num1} + {Num2} = {Num1+Num2}")
