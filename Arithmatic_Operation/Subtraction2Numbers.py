6ccb1429fb16585573ba9cc4a36f44197644dc28
Rudranarayan Sahoo <rudraomm2005@gmail.com>
2024-11-10 19:11:57 +0530

    Add files via upload

---
+++ Arithmatic_Operation/Subtraction2Numbers.py
@@ -0,0 +1,8 @@
+#Arithmatic Operation - Subtraction Two Numbers..
+
+#User-Input: Ask User To Input Two Values
+Num1 = float(input("Enter First Number: "))
+Num2 = float(input("Enter Second Number: "))
+
+# Print The Result
+print(f"{Num1} - {Num2} = {Num1-Num2}")
