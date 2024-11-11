fea6972acbf31c7cf6468f08e48efdf2e98bbd02
Rudranarayan Sahoo <rudraomm2005@gmail.com>
2024-11-11 18:47:42 +0530

    Add files via upload

---
+++ Arithmatic_Operation/Multiplication2Numbers.py
@@ -0,0 +1,8 @@
+#Arithmatic Operation - Multiplication Two Numbers..
+
+# User-Input: ask User For Input Two Values 
+Num1 = float(input("Enter First Number: "))
+Num2 = float(input("Enter Second Number: "))
+
+#print The Result
+print(f"The Multiplication Of {Num1} x {Num2} = {Num1*Num2}")
