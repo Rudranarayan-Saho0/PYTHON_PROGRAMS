b93375d1aaed9a3c8a11c6add7cac3fe1f37abe0
Rudranarayan Sahoo <rudraomm2005@gmail.com>
2024-11-24 18:33:15 +0530

    Add files via upload

---
+++ Even Or Odd/EvenOdd.py
@@ -0,0 +1,7 @@
+#Check An Inputed Number Is Even Or Odd
+number = int(input ("Enter A Number For Check That A Even Or Odd Number: ")) #User Input
+#Check The Number Is Even Or Odd
+if number%2==0:
+    print('The Inputed Number Is A Even Number') #Print If The Number Is Even
+else:
+    print('The Inputed Number Is an Odd Number') #print If The Number Is Odd
