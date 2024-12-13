b8985609569343753e5d2594d4a1d615d85478f4
Rudranarayan Sahoo <rudraomm2005@gmail.com>
2024-12-13 20:32:38 +0530

    Add files via upload

---
+++ poem-Writing/PoemWriting2.py
@@ -0,0 +1,9 @@
+#A Simple Python Program To Print A Poem...
+
+print("\nTwinkle, twinkle, little star,\nHow I wonder what you are!\nUp above the world so high,\nLike a diamond in the sky.")
+print("")
+print("When the blazing sun is gone,\nWhen he nothing shines upon,\nThen you show your little light,\nTwinkle, twinkle, all the night.")
+print("")
+print("Then the traveler in the dark\nThanks you for your tiny spark,\nHow could he see where to go,\nIf you did not twinkle so?")
+print("")
+print("In the dark blue sky you keep,\nOften through my curtains peep,\nFor you never shut your eye,\nTill the sun is in the sky...")
